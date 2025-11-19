0d3050226725a2014b9f2748d453b88b4121619e
AlBukhori <62735792+qonnthecat@users.noreply.github.com>
2025-11-19 20:27:40 +0700

    update js/views/templates/navigation.js

---
+++ js/views/templates/navigation.js
@@ -0,0 +1,27 @@
+// js/views/templates/navigation.js
+export const NavigationTemplate = () => {
+    return `
+        <nav class="bottom-bar">
+            <ul class="bottom-nav">
+                <li class="bottom-nav-item">
+                    <a href="#home" class="bottom-nav-link active">
+                        <span class="bottom-nav-icon">🏠</span>
+                        <span class="bottom-nav-text" data-i18n="navigation.home">Home</span>
+                    </a>
+                </li>
+                <li class="bottom-nav-item">
+                    <a href="#features" class="bottom-nav-link">
+                        <span class="bottom-nav-icon">📋</span>
+                        <span class="bottom-nav-text" data-i18n="navigation.features">Features</span>
+                    </a>
+                </li>
+                <li class="bottom-nav-item">
+                    <a href="#settings" class="bottom-nav-link">
+                        <span class="bottom-nav-icon">⚙️</span>
+                        <span class="bottom-nav-text" data-i18n="navigation.settings">Settings</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    `;
+};
