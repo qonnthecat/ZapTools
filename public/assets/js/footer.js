0c2754fcc889b51781f9f738edb8130273857f95
AlBukhori <62735792+qonnthecat@users.noreply.github.com>
2025-11-19 16:35:46 +0700

    update public/assets/js/footer.js

---
+++ public/assets/js/footer.js
@@ -0,0 +1,7 @@
+export function loadFooter() {
+  document.getElementById("footer").innerHTML = `
+    <footer>
+      <p>© 2025 DuniaBercerita — Dibangun dengan cinta komunitas.</p>
+    </footer>
+  `;
+}
