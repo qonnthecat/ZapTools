dec1bc1208fed0a14bf76372f0ed4ed5a81dc274
AlBukhori <62735792+qonnthecat@users.noreply.github.com>
2025-11-19 20:34:56 +0700

    update js/views/templates/footer.js

--- js/views/templates/footer.js
+++ js/views/templates/footer.js
@@ -1,13 +1,12 @@
 // js/views/templates/footer.js
+import { LogoComponent } from '../components/logo.js';
+
 export const FooterTemplate = () => {
     return `
         <footer class="footer">
             <div class="container">
                 <div class="footer-content">
-                    <div class="footer-logo">
-                        <img src="images/logos/transparent.png" alt="ZapTools" class="footer-logo-image">
-                        <span data-i18n="app.title">ZapTools</span>
-                    </div>
+                    ${LogoComponent.renderFooter()}
                     <p data-i18n="app.copyright">© 2023 ZapTools. All rights reserved.</p>
                 </div>
             </div>
