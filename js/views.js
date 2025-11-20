53f8193ddc02581dce07b12d4e6189a996af3a50
AlBukhori <62735792+qonnthecat@users.noreply.github.com>
2025-11-20 18:55:15 +0700

    update js/views.js

--- js/views.js
+++ js/views.js
@@ -1,83 +1,20 @@
-// ----------------------------
-// Views registry untuk ZapTools MVVM + Alpine.js
-// ----------------------------
+// views.js
+// Optional helper for global view utilities
 
-// Import Templates
-import { HomeTemplate } from "./views/templates/home.js";
-import { ToolsTemplate } from "./views/templates/tools.js";
-import { ConverterTemplate } from "./views/templates/converter.js";
-import { HistoryTemplate } from "./views/templates/history.js";
-import { FeaturesTemplate } from "./views/templates/features.js";
-import { PasswordGeneratorTemplate } from "./views/templates/password-generator.js";
-import { ImageConverterTemplate } from "./views/templates/image-converter.js";
-import { ColorToolsTemplate } from "./views/templates/color-tools.js";
-import { SettingsTemplate } from "./views/templates/settings.js";
-import { TextConverterTemplate } from "./views/templates/text-converter.js";
-
-// Import ViewModels
-import { HomeViewModel } from "./viewmodels/home.vm.js";
-import { ToolsViewModel } from "./viewmodels/tools.vm.js";
-import { ConverterViewModel } from "./viewmodels/converter.vm.js";
-import { HistoryViewModel } from "./viewmodels/history.vm.js";
-import { FeaturesViewModel } from "./viewmodels/features.vm.js";
-import { PasswordGeneratorViewModel } from "./viewmodels/password-generator.vm.js";
-import { ImageConverterViewModel } from "./viewmodels/image-converter.vm.js";
-import { ColorToolsViewModel } from "./viewmodels/color-tools.vm.js";
-import { SettingsViewModel } from "./viewmodels/settings.vm.js";
-import { TextConverterViewModel } from "./viewmodels/text-converter.vm.js";
-
-// ----------------------------
-// Daftar route => { template, viewmodel }
-// ----------------------------
-
-export const views = {
-    "/home": {
-        template: HomeTemplate,
-        viewmodel: HomeViewModel
+export const Views = {
+    /**
+     * Load a template by name (used by router)
+     */
+    async loadTemplate(name) {
+        const module = await import(`./views/templates/${name}.js`);
+        return module.default;
     },
 
-    "/tools": {
-        template: ToolsTemplate,
-        viewmodel: ToolsViewModel
-    },
-
-    "/converter": {
-        template: ConverterTemplate,
-        viewmodel: ConverterViewModel
-    },
-
-    "/history": {
-        template: HistoryTemplate,
-        viewmodel: HistoryViewModel
-    },
-
-    "/features": {
-        template: FeaturesTemplate,
-        viewmodel: FeaturesViewModel
-    },
-
-    "/password-generator": {
-        template: PasswordGeneratorTemplate,
-        viewmodel: PasswordGeneratorViewModel
-    },
-
-    "/image-converter": {
-        template: ImageConverterTemplate,
-        viewmodel: ImageConverterViewModel
-    },
-
-    "/color-tools": {
-        template: ColorToolsTemplate,
-        viewmodel: ColorToolsViewModel
-    },
-
-    "/settings": {
-        template: SettingsTemplate,
-        viewmodel: SettingsViewModel
-    },
-
-    "/text-converter": {
-        template: TextConverterTemplate,
-        viewmodel: TextConverterViewModel
+    /**
+     * Render HTML into the #app container
+     */
+    render(html) {
+        const root = document.getElementById("app");
+        if (root) root.innerHTML = html;
     }
 };
